9cabd7d926736f82620fee2b238e14fb767ac425
Shani <shani.gilad@post.runi.ac.il>
2023-12-21 21:41:05 +0200

    task 1

--- AddTwo.java
+++ AddTwo.java
@@ -5,11 +5,8 @@ public class AddTwo {
 	public static void main(String[] args) {
 		int c = Integer.parseInt (args[0]);
 		int d = Integer.parseInt (args[1]);
-		int a = Integer.parseInt (args[2]);
-		int b = Integer.parseInt (args[3]);
-
+		
 		System.out.println(c + " + " + d + " = " + (c + d));
-		System.out.println(a + " + " + b + " = " + (a + b));
 
 	}
 }
