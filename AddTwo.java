0c854c99a8db686a5821431a3b919e79c0f48dcd
Shani <shani.gilad@post.runi.ac.il>
2023-12-21 21:38:32 +0200

    question 1

--- AddTwo.java
+++ AddTwo.java
@@ -3,12 +3,13 @@
  */
 public class AddTwo {
 	public static void main(String[] args) {
-		int a = 3 ;
-		int b = 5 ;
-		System.out.println(a + " + " + b + " = " + (a + b));
-		int c = 9281 ;
-		int d = 719 ;
+		int c = Integer.parseInt (args[0]);
+		int d = Integer.parseInt (args[1]);
+		int a = Integer.parseInt (args[2]);
+		int b = Integer.parseInt (args[3]);
+
 		System.out.println(c + " + " + d + " = " + (c + d));
+		System.out.println(a + " + " + b + " = " + (a + b));
 
 	}
 }
