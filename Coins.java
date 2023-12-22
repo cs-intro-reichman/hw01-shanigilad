c194a25f3f5aa8504327a8d2ba017d7e9f303834
Shani <shani.gilad@post.runi.ac.il>
2023-12-22 16:25:18 +0200

    task2

--- Coins.java
+++ Coins.java
@@ -8,6 +8,6 @@ public class Coins {
 		int num = Integer.parseInt (args[0]); //change the args to numbers 
 		int numq = (num / 25); // number of coins that can be represented in quarters
 		int numc = (num % 25); // number of coins that can not be represented in quarters
-			System.out.println ("use " + numq + " quarters and " +  numc  + " cents");
+			System.out.println ("Use " + numq + " quarters and " +  numc  + " cents");
 	}
 }
