84785847954927f67fd03fa4090cb8173265c2a7
Shani <shani.gilad@post.runi.ac.il>
2023-12-22 16:19:16 +0200

    q2

--- Coins.java
+++ Coins.java
@@ -4,6 +4,7 @@
  */
 public class Coins {
 	public static void main(String[] args) {
+		// put your code here
 		int num = Integer.parseInt (args[0]); //change the args to numbers 
 		int numq = (num / 25); // number of coins that can be represented in quarters
 		int numc = (num % 25); // number of coins that can not be represented in quarters
